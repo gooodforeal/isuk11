9dc3fa92bd0ec81813adc397db762b5df60dfa1f
gooodforeal <2390603@gmail.com>
2023-04-23 17:32:25 +0300

    2

--- task2/main.py
+++ task2/main.py
@@ -2,6 +2,10 @@ import pandas as pd
 from datetime import datetime
 from docx import Document
 import re
+import os
+from barcode.writer import ImageWriter
+from barcode.codex import Code128
+from docx2pdf import convert
 
 
 def replace_text(paragraph, key, value):
@@ -24,9 +28,7 @@ def read_data():
         data[row["Грузополучатель"]]["{{FROM}}"] = row["Грузоотправитель"]
         data[row["Грузополучатель"]]["{{DOCUMENT}}"] = row["Основание"]
         data[row["Грузополучатель"]]["{{DATE}}"] = datetime.today().date().strftime("%d.%m.%y")
-        data[row["Грузополучатель"]]["Позиции"].append([len(data[row["Грузополучатель"]]["Позиции"]) + 1, row["Товар"], row["Ед."], row["Цена"], row["Кол-во"], row["Сумма"]])
-        data[row["Грузополучатель"]]["{{GLOBAL_PRICE}}"] = sum(el[5] for el in data[row["Грузополучатель"]]["Позиции"])
-        data[row["Грузополучатель"]]["{{GLOBAL_COUNT}}"] = len(data[row["Грузополучатель"]]["Позиции"])
+        data[row["Грузополучатель"]]["Позиции"].append([len(data[row["Грузополучатель"]]["Позиции"]) + 1, row["Товар"], row["Ед."],  row["Кол-во"]])
         data[row["Грузополучатель"]]["{{DOCUMENT}}"] = row["Основание"]
     return data
 
@@ -38,8 +40,14 @@ def main():
     template = 'template.docx'
     n = 1
     for key, value in data.items():
-        result = f'накладная_{n:02}_{key}.docx'
+        result = f'накладная_{n:02}_{key}'
         template_doc = Document(template)
+        barcode_1 = Code128(f'AB{data[key]["{{NUMBER}}"]}_{data[key]["{{DATE}}"]}', writer=ImageWriter(format='PNG'))
+        filename = barcode_1.save('example', options=dict(module_height=3))
+        par = template_doc.paragraphs[0].insert_paragraph_before()
+        lr = par.add_run()
+        lr.add_picture('example.PNG')
+        os.remove('example.PNG')
         for k, v in value.items():
             if re.fullmatch(pattern, k.strip()):
                 for paragraph in template_doc.paragraphs:
@@ -51,8 +59,11 @@ def main():
             for cell in row.cells:
                 cell.text = str(line[i])
                 i += 1
-        template_doc.save(f"result/{result}")
+        template_doc.save(f"result/{result}.docx")
+        convert(f"result/{result}.docx", f"result/{result}.pdf")
+        os.remove(f"result/{result}.docx")
         n += 1
+
 
 if __name__ == "__main__":
     main()
